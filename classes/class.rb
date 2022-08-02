887d0e9236f198eb0e405a43a975e9bb17d925a1
frank1738 <franklineosoro08@gmail.com>
2022-08-02 13:34:55 +0300

    modify the game class

--- classes/class.rb
+++ classes/class.rb
@@ -1,8 +1,8 @@
 class Game
   attr_accessor :multiplayer, :last_played_at
 
-  def initialize(_multiplayer, last_played_at)
-    @multi_player = multi_player
+  def initialize(multiplayer, last_played_at)
+    @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 end
