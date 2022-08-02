bd649d01cb1794c8e1fd825bcd953d32a648a661
frank1738 <franklineosoro08@gmail.com>
2022-08-02 16:00:20 +0300

    add methods for game and author classes

--- classes/item.rb
+++ classes/item.rb
@@ -2,14 +2,18 @@ class Item
   attr_accessor :label, :publish_date, :genre, :author, :source
   attr_reader :id, :archived
 
-  def initialize(publish_date:, id:, archived: false)
-    @id = id || Random.rand(1..1_000_000)
-    @publish_date = Date.parse(publish_date)
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(publish_date, archived = false, id = rand(1..100_000))
+    super()
+    @id = id
+    @publish_date = publish_date
     @archived = archived
   end
 
   def can_be_archived?
-    DateTime.now.year - @published_date.year > 10
+    current = Time.now.year
+    difference = current - publish_date.to_i
+    difference > 10
   end
 
   def move_to_archive
