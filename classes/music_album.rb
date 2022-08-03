4da9980d17846ee02ca09778478b2e129020946d
[Dinma-Faith] <[phaytep@gmail.com]>
2022-08-03 13:21:01 +0100

    Add tests for music-album and genre

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -4,8 +4,8 @@ require 'date'
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify: true)
-    super(id: 50, publish_date: Date.today.to_s)
+  def initialize(publish_date, on_spotify: true)
+    super(publish_date)
     @on_spotify = on_spotify
   end
 
@@ -13,3 +13,9 @@ class MusicAlbum < Item
     super && @on_spotify
   end
 end
+
+# date = Date.today
+# p Date.parse(date)
+
+# music_album = MusicAlbum.new
+# p music_album.can_be_archived?
