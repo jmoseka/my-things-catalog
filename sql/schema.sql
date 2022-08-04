61d586f843a5cd365587f83ce3d0e4945ac1dc9c
jmoseka <jamylam3@gmail.com>
2022-08-04 11:01:06 +0300

    Add item table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,15 @@
+CREATE TABLE item (
+    id BIGSERIAL PRIMARY KEY,
+    publish_date DATE,
+    archived BOOLEAN,
+    genre_id INTEGER,
+    author_id INTEGER,
+    label_id INTEGER,
+    CONSTRAINT fkey_label FOREIGN KEY (label_id) REFERENCES label (id),
+    CONSTRAINT fkey_genre FOREIGN KEY (genre_id) REFERENCES genres (id),
+    CONSTRAINT fkey_author FOREIGN KEY (author_id) REFERENCES author (id)
+);
+
 CREATE TABLE author (
     id INTEGER GENERATED ALWAYS AS IDENTITY,
     first_name VARCHAR NOT NULL,
@@ -16,16 +28,16 @@ CREATE TABLE game (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE label (
+id BIGSERIAL PRIMARY KEY,
+title VARCHAR(100),
+color VARCHAR(50),
+items INT []
+);
+
 CREATE TABLE Book (
 id BIGSERIAL PRIMARY KEY,
 publisher VARCHAR(100),
 cover_date VARCHAR(100),
 FOREIGN KEY(id) REFERENCES item(id)
-);
-
-CREATE TABLE label (
-id BIGSERIAL PRIMARY KEY,
-title VARCHAR(100),
-color VARCHAR(50)
-items INT []
 );
