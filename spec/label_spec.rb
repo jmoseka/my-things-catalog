30a732f662c36ead5d9d6d0dd612820c5b770b9f
jmoseka <jamylam3@gmail.com>
2022-08-05 10:23:28 +0300

    Test for label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../classes/label'
+require_relative '../classes/book'
 
 describe 'Label object' do
   context 'should create an instance of Label class' do
     label = Label.new('Cinderella', 'Purple')
+    book = Book.new('Cindy Lohan', 'good', '2022/02/12')
 
     it 'takes three parameters and returns a Label object' do
       expect(label.class) == Label
@@ -14,6 +16,11 @@ describe 'Label object' do
 
       expect(label.title).not_to eq 'Cindy'
       expect(label.color).not_to eq 'Red'
+    end
+
+    it 'should add an item to label class item array' do
+      label.add_item(book)
+      expect(label.items.length).to eq 1
     end
   end
 end
